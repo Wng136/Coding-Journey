277016638813d4d717edf8ab586a1a50b35f82fb
Wng136 <louiswng136@gmail.com>
2026-07-24 19:24:17 +0800

    Update 02_Output_Exercises.py

--- Python/01_Fundamentals/02_Output/02_Output_Exercises.py
+++ Python/01_Fundamentals/02_Output/02_Output_Exercises.py
@@ -1,13 +1,22 @@
-##Exercise 1 — Print your information
-
-Print:
+"""
+Exercise 1 — Print your information
 
 Name: John
 Age: 20
 Country: Malaysia
 
 Don't copy the example exactly—replace it with your own information.
+"""
 
+Name = "Louis"
+Age = 20
+Country = "Malaysia"
+
+print("Hello, my name is", Name, ". I'm", Age, "this year and I live in", Country, ".")
+
+
+
+"""
 Exercise 2 — Multiple print()
 
 Using three print() statements, display:
@@ -15,6 +24,15 @@ Using three print() statements, display:
 Python
 is
 awesome!
+"""
+
+print("Python")
+print("is")
+print("awesome!")
+
+
+
+"""
 Exercise 3 — Printing numbers
 
 Without quotation marks, print:
@@ -22,6 +40,15 @@ Without quotation marks, print:
 100
 2500
 2026
+"""
+
+print(100)
+print(2500)
+print(2026)
+
+
+
+"""
 Exercise 4 — Basic calculations
 
 Print the answers for:
@@ -32,16 +59,28 @@ Print the answers for:
 20 / 4
 
 Only use print().
+"""
 
+print(10 + 15)
+print(30 - 8)
+print(6 * 9)
+print(20 / 4)
+
+
+
+"""
 Exercise 5 — Mix text and numbers
 
 Produce:
 
 There are 7 days in a week.
+"""
 
-Hint:
+print("There are", 7, "days in a week")
 
-print("There are", 7, "days in a week.")
+
+
+"""
 Exercise 6 — More calculations
 
 Output should be
@@ -49,9 +88,14 @@ Output should be
 5 + 5 = 10
 
 using only one print statement.
+"""
 
-(Hint: mix text and math.)
+print( 5, "+", 5, "=", 5+5)
+print( "5 + 5 =", 5+5)
 
+
+
+"""
 Exercise 7 — Study log
 
 Print
@@ -63,21 +107,28 @@ Then print
 Today is Day 2 of learning Python.
 
 without manually typing 2.
+"""
 
+Day = 1
+print("Today is Day", Day, "of learning Python.")
+print("Today is Day", Day + 1, "of learning Python.")
+
+
+
+"""
 Exercise 8 — Predict the output
 
 Without running the code, write down what you think each line prints.
 
-print(10 + 2)
+print(10 + 2) #12
 
-print("10" , 2)
+print("10" , 2) #10 2
 
-print("Python", 3 * 2)
+print("Python", 3 * 2) #Python 6
 
-print(9 / 3)
+print(9 / 3) #3.0
 
-print("Day", 2 + 4)
+print("Day", 2 + 4) #Day 6
 
 Then run it to check yourself.
-
-This is a surprisingly effective way to learn.
+"""
