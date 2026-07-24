adca5248635fbcb1a8305162e74cd2306c5e0198
Wng136 <louiswng136@gmail.com>
2026-07-24 15:12:25 +0800

    Update Begining.py

--- Python/01_Fundamentals/Begining.py
+++ Python/01_Fundamentals/Begining.py
@@ -1,2 +1,12 @@
-""" 
+"""
+Hello!
+I guess the creation of this file represents
+the start of my journey in building a foundation in python coding,
+in hopes of advancing towards a deeper understanding and programming skill development.
 
+All this is ultimately for the preperation and training to be accustomed to the programming world.
+I hope that this effort would hold great importance and bear meaningful fruits in my path towards becoming a competant engineer.
+
+And with that, I formally present my greetings to the world.
+"""
+print("Hello, Magnificent World!")
